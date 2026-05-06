3d3d0d273a0d11dcfebe0d363cdb90715b282b74
Ivan <imivanex@gmail.com>
2026-05-06 17:47:42 +0200

    Create AttackUnit, DefenseUnit, SpecialUnit
    - Creación de las clases de AttackUnit, DefenseUnit y SpecialUnit.
    - Corrección del metodo getExperience que devolvía un void y debería devolver un int.

---
+++ src/m3/DefenseUnit.java
@@ -0,0 +1,33 @@
+package m3;
+
+public abstract class DefenseUnit {
+	private int armor, initialArmor, baseDamage, experience;
+	private boolean sanctified;
+	
+	
+	// Resta receivedDamage a la armadura de la unidad
+	public void takeDamage(int receivedDamage) {
+		this.armor = this.armor - receivedDamage;
+	};
+	
+	// Devuelve la armadura actual de la unidad
+	public int getActualArmor() {
+		return this.armor;
+	};
+	
+	// Reestablece la armadura actual a la armadura inicial
+	public void resetArmor() {
+		this.armor = initialArmor;
+	};
+	
+	// Suma n a la experiencia actual de la unidad
+	public void gainExperience(int n) {
+		this.experience = this.experience + n;
+	};
+
+	// Devuelve la experiencia actual de la unidad
+	public int getExperience() {
+		return this.experience;
+	};
+	
+}
---
+++ src/m3/SpecialUnit.java
@@ -0,0 +1,31 @@
+package m3;
+
+public abstract class SpecialUnit implements MilitaryUnit, Variables {
+	private int armor, initialArmor, baseDamage, experience;
+	
+	// Resta receivedDamage a la armadura de la unidad
+	public void takeDamage(int receivedDamage) {
+		this.armor = this.armor - receivedDamage;
+	};
+	
+	// Devuelve la armadura actual de la unidad
+	public int getActualArmor() {
+		return this.armor;
+	};
+	
+	// Reestablece la armadura actual a la armadura inicial
+	public void resetArmor() {
+		this.armor = initialArmor;
+	};
+	
+	// Suma n a la experiencia actual de la unidad
+	public void gainExperience(int n) {
+		this.experience = this.experience + n;
+	};
+
+	// Devuelve la experiencia actual de la unidad
+	public int getExperience() {
+		return this.experience;
+	};
+	
+}
